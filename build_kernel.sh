6cb5e1a750c3cc8f27068955893a38e9e7925b1c
travarilo <travarilo@yahoo.com>
2016-11-16 16:41:46 +0700

    Building Update

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,12 +1,4 @@
 DEFCONFIG=cyanogen_cancro_defconfig
-make ARCH=arm CROSS_COMPILE=~/toolchain/arm-eabi-5.3/bin/arm-eabi- $DEFCONFIG
-make -j32 ARCH=arm CROSS_COMPILE=~/toolchain/arm-eabi-5.3/bin/arm-eabi-
-for i in `find -name *.ko`; do cp $i ~/anykernel/AnyKernel2/modules/; done
-dtbToolCM -s 2048 -d "qcom,msm-id = <" -2 -o arch/arm/boot/dt.img -p /usr/bin/ arch/arm/boot/
-cp arch/arm/boot/zImage ~/anykernel/AnyKernel2/
-cp arch/arm/boot/dt.img ~/anykernel/AnyKernel2/
-cd ~/anykernel/AnyKernel2
-DATE=$(date +"%m-%d-%y")
-rm *.zip
-zip -r9 ZeurionX-$DATE.zip * -x README ZeurionX-$DATE.zip
-sudo cp Z* ~/public_html/
+make ARCH=arm CROSS_COMPILE=~/android/SMxU/bin/arm-eabi- $DEFCONFIG
+make ARCH=arm CROSS_COMPILE=~/android/SMxU/bin/arm-eabi-
+./dtbTool -o arch/arm/boot/dt.img -s 2048 -p ./scripts/dtc/ ./arch/arm/boot/
